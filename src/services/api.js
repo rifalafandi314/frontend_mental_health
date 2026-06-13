35df657715c947fb02b195cc7c900ee2e61c695d
rifalafandi314 <rifalafandi314@gmail.com>
2026-06-13 12:13:17 +0700

    add api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Konfigurasi URL backend (FastAPI berjalan di port 8000)
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://3772-2001-448a-8072-4a7c-c1b0-e3b7-c293-9e4c.ngrok-free.app';
 
 export const predictStressLevel = async (data) => {
   try {
